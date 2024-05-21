22d8ae8727e685755789382ef1f5900b8927b8bc
Parcours <devaletpaul@gmail.com>
2024-05-21 10:48:43 +0200

    Test 2

--- ProjetPizza/src/main/java/com/pizzaapp/controllers/HomeServlet.java
+++ ProjetPizza/src/main/java/com/pizzaapp/controllers/HomeServlet.java
@@ -1,5 +1,15 @@
 package com.pizzaapp.controllers;
 
-public class HomeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("jsp/home.jsp").forward(request, response);
+    }
 }
