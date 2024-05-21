a85ef66f8945dd4e370c636575ea0c24661fec92
Parcours <devaletpaul@gmail.com>
2024-05-21 11:07:42 +0200

    Fix erreur start serveur

--- ProjetPizza/src/main/java/com/pizzaapp/controllers/AccountServlet.java
+++ ProjetPizza/src/main/java/com/pizzaapp/controllers/AccountServlet.java
@@ -11,15 +11,17 @@ import java.io.IOException;
 
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             response.sendRedirect("login.jsp");
         } else {
-            request.getRequestDispatcher("jsp/account.jsp").forward(request, response);
+            request.getRequestDispatcher("/jsp/account.jsp").forward(request, response);
         }
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
         String address = request.getParameter("address");
@@ -27,6 +29,6 @@ public class AccountServlet extends HttpServlet {
 
         User user = new User(name, address, phone);
         request.getSession().setAttribute("user", user);
-        request.getRequestDispatcher("jsp/account.jsp").forward(request, response);
+        request.getRequestDispatcher("/jsp/account.jsp").forward(request, response);
     }
 }
